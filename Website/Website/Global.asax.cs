71b85d06b9c19bd673b17158a474bdc8fff0cd7f
lucykur <lucykur@thoughtworks.com>
2011-05-21 11:46:45 +0530

    #0000 - Introducing routes <Hari/Lucy>

--- Website/Website/Global.asax.cs
+++ Website/Website/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Website
@@ -19,14 +15,10 @@ namespace Website
 
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
+            routes.MapPageRoute("scripts", "scripts", "~/Scripts");
+            routes.MapPageRoute("content", "content", "~/Content");
+            routes.MapRoute("home", "home/{action}", new {Controller = "Home", action = "Login"});
+            routes.MapRoute("Default", "{controller}/{action}", new {Controller = "Home", action="Index"});
         }
 
         protected void Application_Start()
