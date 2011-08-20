32610a2824f4afaaacdb6c0d28adc2c79e1ac6e8
harib <linkwithhari@gmail.com>
2011-08-20 15:55:03 +0530

    #000 - temp commit

--- Website/Website/Scripts/verticaltabs/verticaltabs.js
+++ Website/Website/Scripts/verticaltabs/verticaltabs.js
@@ -1,11 +1,21 @@
-﻿ <script type="text/javascript">
-        $(function() {
-            var $items = $('#vtab>ul>li');
-            $items.mouseover(function() {
-                $items.removeClass('selected');
-                $(this).addClass('selected');
-                var index = $items.index($(this));
-                $('#vtab>div').hide().eq(index).show();
-            }).eq(1).mouseover();
-        });
-    </script>
+﻿ <script type="text/javascript">
+
+        $(function() {
+
+            var $items = $('#vtab>ul>li');
+
+            $items.mouseover(function() {
+
+                $items.removeClass('selected');
+
+                $(this).addClass('selected');
+
+                var index = $items.index($(this));
+
+                $('#vtab>div').hide().eq(index).show();
+
+            }).eq(1).mouseover();
+
+        });
+
+    </script>
