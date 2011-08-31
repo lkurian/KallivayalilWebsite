fbf988ee9cbc549c3f0fa0b07567fe20987fdaf1
lucykur <kallivayalil.family@gmail.com>
2011-08-31 14:14:40 +0530

    Introducing the service call for search <Lucy>

--- Website/Website/Controllers/SearchController.cs
+++ Website/Website/Controllers/SearchController.cs
@@ -1,10 +1,17 @@
+using System.Configuration;
 using System.Web.Mvc;
+using Kallivayalil.Client;
+using Website.Helpers;
+using Website.Models;
 using Website.Models.ViewModels;
 
 namespace Website.Controllers
 {
     public class SearchController : Controller
     {
+        private AutoDataContractMapper mapper = new AutoDataContractMapper();
+        private string serviceBaseUri = ConfigurationManager.AppSettings["serviceBaseUri"];
+
         public ActionResult Index()
         {
 //            if(Session["userName"]==null)
@@ -14,9 +21,13 @@ namespace Website.Controllers
 
 
         [HttpPost]
-        public ActionResult Search(SearchModel strings)
+        public ActionResult Search(SearchModel searchCriteria)
         {
+            var constituentsData = HttpHelper.Get<ConstituentsData>(string.Format(serviceBaseUri + "/Search?firstname={0}&lastname={1}", searchCriteria.FirstName,searchCriteria.LastName));
 
+            mapper = new AutoDataContractMapper();
+            var constituents = new Constituents();
+            mapper.MapList(constituentsData, constituents,typeof(Constituent));
             return View("Index");
         }
     }
