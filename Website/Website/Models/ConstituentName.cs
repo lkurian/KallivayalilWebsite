1f4b627307da826e9f6ebe939b403a3b715b30c6
lucykur <kallivayalil.family@gmail.com>
2011-09-14 19:31:59 +0530

    Search with relationships <lucy>

--- Website/Website/Models/ConstituentName.cs
+++ Website/Website/Models/ConstituentName.cs
@@ -10,6 +10,15 @@ namespace Website.Models
         public virtual string PreferedName { get; set; }
         public virtual SalutationType Salutation { get; set; }
 
+        public virtual string NameString
+        {
+            get { return ToString(); }
+        }
 
+
+        public override string ToString()
+        {
+            return string.Format("{3}.{0} {1} {2}", FirstName, MiddleName, LastName, Salutation.Description);
+        }
     }
 }
